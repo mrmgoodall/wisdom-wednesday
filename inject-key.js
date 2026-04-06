5badc743da30ad0e058f779450caa1905e268454
mrmgoodall <mrmgoodall@icloud.com>
2026-04-06 09:05:24 -0600

    Update inject-key.js

--- inject-key.js
+++ inject-key.js
@@ -1,4 +1,4 @@
-javascriptconst fs = require('fs');
+const fs = require('fs');
 const key = process.env.ANTHROPIC_API_KEY || '';
 let html = fs.readFileSync('index.html', 'utf8');
 html = html.replace('REPLACE_API_KEY', key);
